be8e39f8247522b7950e7715d337e02762f351eb
Vadim Musin <wasd94@gmail.com>
2024-05-07 01:41:14 +0200

    Fix tests

--- tests/test_capture_warnings_ng.py
+++ tests/test_capture_warnings_ng.py
@@ -1,51 +1,49 @@
 
-TWO_WARNINGS_FOUND_LINE = '*Total warnings written to test_warnings.txt: 2*'
+WARNINGS_FOUND_LINE = '*Total warnings written to test_warnings.txt: 32*'
 WARNINGS_NOT_FOUND_LINE = '*No warnings to write.*'
 
-# def test_help_message(pytester):
-#     result = pytester.runpytest(
-#         '--help',
-#     )
-#     # fnmatch_lines does an assertion internally
-#     result.stdout.fnmatch_lines([
-#         'capture-warnings-ng:',
-#         '*--foo=DEST_FOO*Set the value for the fixture "bar".',
-#     ])
+PYFILE_WITH_WARNINGS = """
+    import pytest
+    import warnings
 
+    @pytest.mark.parametrize("x", range(0,16))
+    def test_hello_world_1(x):
+        warnings.warn('warning 1')
+        warnings.warn('warning 2')
+"""
+
+PYFILE_WITHOUT_WARNINGS = """
+    import pytest
+    import warnings
+
+    @pytest.mark.parametrize("x", range(0,16))
+    def test_hello_world_1(x):
+        pass
+"""
 
 def test_run_with_warnings(pytester):
-    pytester.makepyfile("""
-        import pytest
-        import warnings
-
-        def test_hello_world_1(request):
-            warnings.warn('hello 1 world')
-
-        def test_hello_world_2(request):
-            warnings.warn('hello 2 world')
-    """)
-
+    pytester.makepyfile(PYFILE_WITH_WARNINGS)
     result = pytester.runpytest('-v')
 
-    # fnmatch_lines does an assertion internally
     result.stdout.fnmatch_lines([
-        TWO_WARNINGS_FOUND_LINE,
+        WARNINGS_FOUND_LINE,
     ])
 
-    # make sure that we get a '0' exit code for the testsuite
     assert result.ret == 0
 
+def test_run_with_warnings_xdist(pytester):
+    pytester.makepyfile(PYFILE_WITH_WARNINGS)
+    result = pytester.runpytest('-v', '-n', 'auto')
+
+    result.stdout.fnmatch_lines([
+        WARNINGS_FOUND_LINE,
+    ])
+
+    assert result.ret == 0
+
+
 def test_run_without_warnings(pytester):
-    pytester.makepyfile("""
-        import pytest
-        import warnings
-
-        def test_hello_world_1(request):
-            pass
-
-        def test_hello_world_2(request):
-            pass
-    """)
+    pytester.makepyfile(PYFILE_WITHOUT_WARNINGS)
 
     result = pytester.runpytest('-v')
 
@@ -57,39 +55,9 @@ def test_run_without_warnings(pytester):
     # make sure that we get a '0' exit code for the testsuite
     assert result.ret == 0
 
-def test_run_xdist_with_warnings(pytester):
-    pytester.makepyfile("""
-        import pytest
-        import warnings
 
-        def test_hello_world_1(request):
-            warnings.warn('hello 1 world')
-
-        def test_hello_world_2(request):
-            warnings.warn('hello 2 world')
-    """)
-
-    result = pytester.runpytest('-v', '-n', 'auto')
-
-    # fnmatch_lines does an assertion internally
-    result.stdout.fnmatch_lines([
-        TWO_WARNINGS_FOUND_LINE,
-    ])
-
-    # make sure that we get a '0' exit code for the testsuite
-    assert result.ret == 0
-
-def test_run_xdist_without_warnings(pytester):
-    pytester.makepyfile("""
-        import pytest
-        import warnings
-
-        def test_hello_world_1(request):
-            pass
-
-        def test_hello_world_2(request):
-            pass
-    """)
+def test_run_without_warnings_xdist(pytester):
+    pytester.makepyfile(PYFILE_WITHOUT_WARNINGS)
 
     result = pytester.runpytest('-v', '-n', 'auto')
 
